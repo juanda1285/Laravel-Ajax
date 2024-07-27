e77e26d22fd08812cb0d4205953e126b4c7e177d
juanda1285 <67526249+juanda1285@users.noreply.github.com>
2024-07-27 16:17:05 -0500

    web.php

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,11 @@
 
 use App\Http\Controllers\UserController;
 
+//Ruta por defecto
 Route::get('/', function () {
     return view('register');
 });
 
+//Ruta y Controlador para el registro
 Route::post('/register', [UserController::class, 'store'])->name('register.store');
 
