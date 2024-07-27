160bf823580786e00a0e6e2b660cb3f3ed671d09
juanda1285 <67526249+juanda1285@users.noreply.github.com>
2024-07-27 16:15:49 -0500

    UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,22 +11,23 @@ class UserController extends Controller
 {
     public function store(Request $request)
     {
+        //Se valida que los campos del formulario cumplan los requisitos
         $validator = Validator::make($request->all(), [
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:8|confirmed',
         ]);
-
+        //En caso que falle :
         if ($validator->fails()) {
             return response()->json(['errors' => $validator->errors()], 422);
         }
-
+        //Creación del usuario
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
-
+        //Respuesta
         return response()->json(['success' => 'User registered successfully.'], 201);
     }
 }
